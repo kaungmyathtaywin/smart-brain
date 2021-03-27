0fba0db75162fe6af2f90177208ff25927d1438d
kaungmyathtaywin <kaungmyathtaywin@gmail.com>
2021-03-28 00:26:31 +0630

    Refactor logo into navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
 import "./Navigation.css";
 
 const Navigation = () => {
   return (
     <nav className="nav-bar">
-      <p className="f3 link dim black pa2 pointer">Sign Out</p>
+      <Tilt className="Tilt br2 shadow-2 ma3" options={{ max: 25 }}>
+        <div className="Tilt-inner">
+          <img src={brain} alt="logo" className="logo" />
+        </div>
+      </Tilt>
+      <p className="f4 link dim black pa2 pointer mr3">Sign Out</p>
     </nav>
   );
 };
