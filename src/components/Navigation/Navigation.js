e826ce8b578e7716a181939fe8566b54360d3c37
kaungmyathtaywin <kaungmyathtaywin@gmail.com>
2021-03-28 16:08:04 +0630

    Changes navbar styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,25 @@ import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <nav className="nav-bar">
-      <Tilt className="Tilt br2 shadow-2 ma3" options={{ max: 25 }}>
+    <nav>
+      <ul className="nav-bar">
+        <li>
+          <Tilt className="Tilt br2 shadow-2 ma2" options={{ max: 25 }}>
+            <div className="Tilt-inner">
+              <img src={brain} alt="logo" className="logo" />
+            </div>
+          </Tilt>
+        </li>
+        <li className="f4 link dim black pa2 pointer mr3 sign-out shawdow-2 br2">
+          Sign Out
+        </li>
+      </ul>
+      {/* <Tilt className="Tilt br2 shadow-2 ma3" options={{ max: 25 }}>
         <div className="Tilt-inner">
           <img src={brain} alt="logo" className="logo" />
         </div>
       </Tilt>
-      <p className="f4 link dim black pa2 pointer mr3">Sign Out</p>
+      <p className="f4 link dim black pa2 pointer mr3">Sign Out</p> */}
     </nav>
   );
 };
